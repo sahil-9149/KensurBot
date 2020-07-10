c5756d205c86988b7cc03073780089d3ef0df6dc
KenHV <kenhv@null.net>
2020-07-10 15:57:31 +0530

    kensurbot: track experimental branch
    Signed-off-by: KenHV <kenhv@null.net>

--- Dockerfile
+++ Dockerfile
@@ -67,7 +67,7 @@ RUN python3 -m ensurepip \
 #
 # Clone repo and prepare working directory
 #
-RUN git clone -b sql-extended https://github.com/KenHV/KensurBot /root/userbot
+RUN git clone -b experimental https://github.com/KenHV/KensurBot /root/userbot
 RUN mkdir /root/userbot/bin/
 WORKDIR /root/userbot/
 
--- userbot/__init__.py
+++ userbot/__init__.py
@@ -80,7 +80,7 @@ UPSTREAM_REPO_URL = os.environ.get(
     "UPSTREAM_REPO_URL",
     "https://github.com/KenHV/KensurBot.git")
 UPSTREAM_REPO_BRANCH = os.environ.get(
-    "UPSTREAM_REPO_BRANCH", "sql-extended")
+    "UPSTREAM_REPO_BRANCH", "experimental")
 
 # Console verbose logging
 CONSOLE_LOGGER_VERBOSE = sb(os.environ.get("CONSOLE_LOGGER_VERBOSE", "False"))
